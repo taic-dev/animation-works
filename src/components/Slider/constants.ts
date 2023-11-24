c33d7a7418d0104e23567701bd8abe125090b2aa
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-11-24 22:20:28 +0900

    add: constants

---
+++ src/components/Slider/constants.ts
@@ -0,0 +1,13 @@
+export const SLIDER_IMAGE_DATA = [
+  { imageLink: "slider1.jpg", altName: "サービス1" },
+  { imageLink: "slider1.jpg", altName: "サービス2" },
+  { imageLink: "slider1.jpg", altName: "サービス3" },
+  { imageLink: "slider1.jpg", altName: "サービス4" },
+];
+
+export const SLIDER_DATA = [
+  { id: "01", text: "サービス1" },
+  { id: "02", text: "サービス2" },
+  { id: "03", text: "サービス3" },
+  { id: "04", text: "サービス4" },
+];
