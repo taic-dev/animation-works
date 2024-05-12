2c6cff2a5c9939dd305d9f20e96430c2ccf33d69
taic-dev <c.t.o.taishi.0530@gmail.com>
2024-05-12 18:34:04 +0900

    add: sequentialString追加

--- src/pages/works01/ts/animation.ts
+++ src/pages/works01/ts/animation.ts
@@ -1,8 +1,10 @@
 export class Animation {
+  tag: Element | null;
   topTitle: Element | null;
   topText: Element | null;
 
   constructor() {
+    this.tag = document.querySelector(".tag p");
     this.topTitle = document.querySelector(".top__title h2");
     this.topText = document.querySelector(".top__text p");
   }
@@ -19,8 +21,9 @@ export class Animation {
   }
 
   _topAnimation() {
-    if (!this.topTitle || !this.topText) return;
+    if (!this.tag || !this.topTitle || !this.topText) return;
 
+    this._stringToSpan(this.tag, "");
     this._stringToSpan(this.topTitle, "");
     this._stringToSpan(this.topText, " ");
   }
