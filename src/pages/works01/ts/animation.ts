5da7eddc376fef07de410f5617e75e85686d85ca
taic-dev <c.t.o.taishi.0530@gmail.com>
2024-05-12 23:00:40 +0900

    add: IntersectionObserver追加

--- src/pages/works01/ts/animation.ts
+++ src/pages/works01/ts/animation.ts
@@ -1,12 +1,29 @@
 export class Animation {
-  tag: Element | null;
-  topTitle: Element | null;
-  topText: Element | null;
+  observer: IntersectionObserver | undefined;
+  options: IntersectionObserverInit | undefined;
+  sections: Element[] | null;
+  tags: Element[] | null;
+  buildingTitle: Element[] | null;
+  buildingText: Element[] | null;
 
   constructor() {
-    this.tag = document.querySelector(".tag p");
-    this.topTitle = document.querySelector(".top__title h2");
-    this.topText = document.querySelector(".top__text p");
+    this.observer;
+    this.options = {
+      root: null,
+      rootMargin: "-50% 0px",
+      threshold: 0,
+    };
+    this.sections = [...document.querySelectorAll(".section")];
+    this.tags = [...document.querySelectorAll(".tag p")];
+    this.buildingTitle = [...document.querySelectorAll(".building__title h2")];
+    this.buildingText = [...document.querySelectorAll(".building__text p")];
+  }
+
+  _setObserver(
+    callback: IntersectionObserverCallback,
+    options: IntersectionObserverInit | undefined
+  ) {
+    this.observer = new IntersectionObserver(callback, options);
   }
 
   _stringToSpan(element: Element, splitString: string) {
@@ -20,15 +37,36 @@ export class Animation {
     element.innerHTML = newString;
   }
 
-  _topAnimation() {
-    if (!this.tag || !this.topTitle || !this.topText) return;
+  _setAnimation() {
+    if (!this.tags || !this.buildingTitle || !this.buildingText) return;
 
-    this._stringToSpan(this.tag, "");
-    this._stringToSpan(this.topTitle, "");
-    this._stringToSpan(this.topText, " ");
+    this.tags.forEach((v) => {
+      this._stringToSpan(v, "");
+    });
+    this.buildingTitle.forEach((v) => {
+      this._stringToSpan(v, "");
+    });
+    this.buildingText.forEach((v) => {
+      this._stringToSpan(v, " ");
+    });
+  }
+
+  _startAnimation(entries: IntersectionObserverEntry[]) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-active");
+      }
+    });
   }
 
   init() {
-    this._topAnimation();
+    this._setAnimation();
+    this._setObserver(this._startAnimation, this.options);
+
+    this.sections?.forEach((tag) => {
+      this.observer?.observe(tag);
+    });
+
+
   }
 }
