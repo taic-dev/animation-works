773cefe34eca3f9b78d3e2426e4259d014169400
taic-dev <c.t.o.taishi.0530@gmail.com>
2024-05-27 17:09:32 +0900

    不要なファイルの削除

--- src/pages/slider/slider1/hook.ts
+++
@@ -1,4 +0,0 @@
-export const handleAutoplayTimeLeft = (s: any, time: number, progress: number) => {
-  const elem = document.querySelector(".time-circle.is-focus") as HTMLElement;
-  elem.style.setProperty('--progress', String(1 - progress));
-};
