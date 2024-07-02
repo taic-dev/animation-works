f42d752c50228db1c2280d6f454087b1e87d5060
taic-dev <c.t.o.taishi.0530@gmail.com>
2024-07-03 01:54:27 +0900

    add: side scroll animation

--- src/pages/works04/ts/animation.ts
+++ src/pages/works04/ts/animation.ts
@@ -1,19 +1,42 @@
-const contents = [...document.querySelectorAll('.content')];
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const contents = [...document.querySelectorAll(".content")];
 
 const options = {
   root: document.querySelector(".body"),
   rootMargin: "center",
   threshold: 1.0,
-}
+};
 
-function addClassIsActive (entries: IntersectionObserverEntry[]) {
-  entries.forEach( (entry) => {
-    if(entry.isIntersecting) {
-      console.log(entry)
-      entry.target.classList.add('is-active');
+function addClassIsActive(entries: IntersectionObserverEntry[]) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-active");
     }
   });
 }
 
 const observer = new IntersectionObserver(addClassIsActive);
-contents.forEach((element) => observer.observe(element)) 
+contents.forEach((element) => observer.observe(element));
+
+const container = document.querySelector<HTMLElement>(".slider");
+const sliderWrapper = document.querySelector<HTMLElement>(".slider-inner");
+const sliderList = document.querySelector<HTMLElement>(".slider-list");
+const sliderItems = document.querySelectorAll(".slider-item");
+
+gsap.to(sliderItems, {
+  xPercent: -100 * (sliderItems.length - 1),
+  ease: "Power0.easeNone",
+  scrollTrigger: {
+    trigger: container,
+    start: 'top top',
+    pin: true,
+    scrub: true,
+    markers: true,
+    end: () => `+=${sliderWrapper?.offsetWidth * 3}`,
+    anticipatePin: 1,
+    invalidateOnRefresh: true,
+  },
+});
